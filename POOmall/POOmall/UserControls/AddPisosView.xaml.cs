c4e2fb7e2f5814159ab37a42740a2950a326444f
PricciWinPC <pricci1@miuandes.cl>
2017-11-09 22:04:22 -0300

    AddPisosView listo

--- POOmall/POOmall/UserControls/AddPisosView.xaml.cs
+++ POOmall/POOmall/UserControls/AddPisosView.xaml.cs
@@ -23,16 +23,20 @@ namespace POOmall.UserControls
     /// </summary>
     public partial class AddPisosView : UserControl
     {
-        public int[] TiendasArray;
-        public ObservableCollection<int> TiendasCollection;
+        public ObservableCollection<string> TiendasCollection;
+        public ObservableCollection<object[]> Pisos;
+        private int _cuenta;
 
         public AddPisosView()
         {
-            TiendasArray = new int[1];
+            TiendasCollection = new ObservableCollection<string>();
+            Pisos = new ObservableCollection<object[]>();
+            _cuenta = 0;
 
             InitializeComponent();
             ListBoxAreas.ItemsSource = TiendasCollection;
-            
+            ListBox.ItemsSource = Pisos;
+
         }
 
         //https://stackoverflow.com/a/1268648
@@ -50,12 +54,42 @@ namespace POOmall.UserControls
 
         private void BtnGuardar_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            //foreach (var item in TiendasCollection)
+            //{
+            //    MessageBox.Show($"{item}");
+            //}
+            Pisos.Add(new object[] {$"Tienda {_cuenta}", TextBoxAreaPiso.Text.ToString(), TiendasCollection});
+            TextBoxAreaPiso.Text = "";
+            TextBoxAreaPiso.IsEnabled = true;
+            TiendasCollection = new ObservableCollection<string>();
+            ListBoxAreas.ItemsSource = TiendasCollection;
+            _cuenta += 1;
+            if (Pisos.Count > 0)
+            {
+                BtnSiguiente.Visibility = Visibility.Visible;
+            }
         }
 
         private void BtnAddTiendas_OnClick(object sender, RoutedEventArgs e)
         {
-            TiendasCollection.Add(new int());
+            try
+            {
+                var areaPiso = Convert.ToUInt16(TextBoxAreaPiso.Text);
+                TiendasCollection.Add("1");
+                TextBoxAreaPiso.IsEnabled = false;
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Primero debe especificar un area válida para el piso", "Error");
+                TextBoxAreaPiso.Text = "";
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("El área no puede ser negativa", "Error");
+                TextBoxAreaPiso.Text = "";
+            }
+            
         }
     }
 }
